c648f3a230d534457d12ec5b772cfea5eefa0aa0
Ferhat <khalil.ferhat@dxc.com>
2026-04-25 20:06:37 +0100

    Push requested by user

--- artifacts/scratch/list_orgs.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const orgs = await prisma.organization.findMany({ select: { id: true, name: true, slug: true } });
-  console.log(JSON.stringify(orgs, null, 2));
-}
-main().finally(() => prisma.$disconnect());
